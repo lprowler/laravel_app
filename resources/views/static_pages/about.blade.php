7220124c58f405e83a4553b8570391f5ca6fbaf2
happydog <1154586492@qq.com>
2024-05-26 23:32:35 +0800

    static pages

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.default')
+@section('title','about')
+@section('content')
+    <h1>about</h1>
+@stop
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.default')
+@section('title','home')
+@section('content')
+    <h1>home</h1>
+@stop
