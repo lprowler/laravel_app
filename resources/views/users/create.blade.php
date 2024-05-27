32ad6a6e3c43b6688c4aae49c51c80355a189799
happydog <1154586492@qq.com>
2024-05-27 17:49:43 +0800

    add form CSRF verify

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -8,6 +8,7 @@
             </div>
             <div class="card-body">
                 <form method="post" action="{{route('users.store')}}">
+                    {{ csrf_field() }}
                     <div class="form-group">
                         <label for="name">username:</label>
                         <input type="text" name="name" class="form-control" value="{{old('name')}}">
