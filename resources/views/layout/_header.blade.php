4c21a34b5453878a8caef080916b63dd16c5c764
happydog <1154586492@qq.com>
2024-05-27 08:53:41 +0800

    split header and footer views

---
+++ resources/views/layout/_header.blade.php
@@ -0,0 +1,9 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="/">weibo app</a>
+        <ul class="navbar-nav justify-content-end">
+            <li class="nav-item"><a class="nav-link" href="/about">about</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">contact</a></li>
+        </ul>
+    </div>
+</nav>
--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -9,17 +9,13 @@
     <title>@yield('title','weibo app') - Laravel 新手入门教程</title>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-        <a class="navbar-brand" href="/">weibo app</a>
-        <ul class="navbar-nav justify-content-end">
-            <li class="nav-item"><a class="nav-link" href="/about">about</a></li>
-            <li class="nav-item"><a class="nav-link" href="#">contact</a></li>
-        </ul>
-    </div>
-</nav>
+@include('layout._header')
 <div class="container">
-    @yield('content')
+    <div class="offset-md-1 col-md-10">
+        @yield('content')
+        @include('layout._footer')
+
+    </div>
 
 </div>
 </body>
