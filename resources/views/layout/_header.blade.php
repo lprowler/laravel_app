240db3099e87dbf073f3f1274c8889366758562e
happydog <1154586492@qq.com>
2024-05-27 09:07:46 +0800

    named route

--- resources/views/layout/_header.blade.php
+++ resources/views/layout/_header.blade.php
@@ -1,9 +1,9 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-        <a class="navbar-brand" href="/">weibo app</a>
+        <a class="navbar-brand" href="{{route('home')}}">weibo app</a>
         <ul class="navbar-nav justify-content-end">
-            <li class="nav-item"><a class="nav-link" href="/about">about</a></li>
-            <li class="nav-item"><a class="nav-link" href="#">contact</a></li>
+            <li class="nav-item"><a class="nav-link" href="{{route('about')}}">about</a></li>
+            <li class="nav-item"><a class="nav-link" href="{{route('contact')}}">contact</a></li>
         </ul>
     </div>
 </nav>
