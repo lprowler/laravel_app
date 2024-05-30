bb5d61f44668d708a9a272cbab4425b29b0e7490
happydog <1154586492@qq.com>
2024-05-30 22:06:07 +0800

    admin can delete users

--- resources/views/users/_user.blade.php
+++ resources/views/users/_user.blade.php
@@ -1,4 +1,11 @@
 <div class="list-group-item">
     <img src="{{$user->gravatar()}}" alt="{{$user->name}}" width="32" class="mr-3">
     <a href="{{route('users.show',$user)}}">{{$user->name}}</a>
+    @can('destroy',$user)
+        <form class="float-right" action="{{route('users.destroy',$user->id)}}" method="post">
+            {{csrf_field()}}
+            {{method_field('DELETE')}}
+            <button class="btn btn-sm btn-danger delete-btn" type="submit">delete</button>
+        </form>
+    @endcan
 </div>
