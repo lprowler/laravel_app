ace28018fd45ec929198092c078f1bece2f0f045
happydog <1154586492@qq.com>
2024-06-02 17:34:08 +0800

    create data model

--- resources/views/emails/confirm.blade.php
+++ resources/views/emails/confirm.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <h2>感谢您在 {{env('APP_NAME')}} 网站进行注册！</h2>
     <p>请点击下面的链接完成注册：<br/>
-        <a href="{{route('confirm_email',$user->activation_token)}}"></a>
+        <a href="{{route('confirm_email',$user->activation_token)}}" target="_blank">{{route('confirm_email',$user->activation_token)}}</a>
     </p>
     <p>如果不是您本人的操作，请忽略此邮件。</p>
 @stop
