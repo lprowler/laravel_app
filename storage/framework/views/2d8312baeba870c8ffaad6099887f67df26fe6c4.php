c085f9b584a2b936c12964bafeac09dafed36dcd
happydog <1154586492@qq.com>
2024-06-02 18:36:43 +0800

    show user data list

--- storage/framework/views/2d8312baeba870c8ffaad6099887f67df26fe6c4.php
+++ storage/framework/views/2d8312baeba870c8ffaad6099887f67df26fe6c4.php
@@ -3,13 +3,27 @@
 <?php $__env->startSection('content'); ?>
     <div class="row">
         <div class="offset-md-2 col-md-8">
-            <div class="col-md-12">
-                <div class="offset-md-2 col-md-8">
-                    <section class="user_info">
-                        <?php echo $__env->make('shared._user_info',['user'=>$user], \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-                    </section>
-                </div>
-            </div>
+
+            <section class="user_info">
+                <?php echo $__env->make('shared._user_info',['user'=>$user], \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+            </section>
+            <hr>
+            <section class="status">
+                <?php if($statuses->count()>0): ?>
+                    <ul class="list-unstyled">
+                        <?php $__currentLoopData = $statuses; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $status): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                            <?php echo $__env->make('statuses._status', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+                        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                    </ul>
+                    <div class="mt-5">
+                        <?php echo $statuses->render(); ?>
+
+                    </div>
+                <?php else: ?>
+                    <p>没有数据！</p>
+                <?php endif; ?>
+            </section>
+
         </div>
     </div>
 <?php $__env->stopSection(); ?>
